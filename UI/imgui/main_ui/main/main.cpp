b33ab6e6440eb957101e4b28c69af5323983ef56
hawthorc <hawthorc@oregonstate.edu>
2025-04-21 16:17:07 -0700

    Removed unused UI windows and changed emulator window default size

--- UI/imgui/main_ui/main/main.cpp
+++ UI/imgui/main_ui/main/main.cpp
@@ -114,8 +114,6 @@ int main(int, char**)
     //IM_ASSERT(font != nullptr);
 
     // Our state
-    bool show_demo_window = true;
-    bool show_another_window = false;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
     // Main loop
@@ -154,10 +152,6 @@ int main(int, char**)
         ImGui_ImplSDL2_NewFrame();
         ImGui::NewFrame();
 
-        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
-
         {
             // Generate random color
             if (nes.A_changed == true && nes.paused == false) {
@@ -167,7 +161,10 @@ int main(int, char**)
             }
 
             // Render the NES screen
+            ImVec2 defaultSize(800,800);
+            ImGui::SetNextWindowSize(defaultSize, ImGuiCond_FirstUseEver);
             ImGui::Begin("NES Screen");
+            //ImGui::Begin("NES Emulator", nullptr, ImGuiWindowFlags_NoResize;		// don't allow resizing?
             ImVec2 widgetSize = ImGui::GetContentRegionAvail();
 
             uint32_t* framebuffer = nes.getFramebuffer();
@@ -349,39 +346,6 @@ int main(int, char**)
             ImGui::Text("S: [%04x]", nes.cpu.S);
             ImGui::Text("P: [%04x]", nes.cpu.P);
 
-            ImGui::End();
-        }
-
-        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
-        {
-            static float f = 0.0f;
-            static int counter = 0;
-
-            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            //ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-            //ImGui::Checkbox("Another Window", &show_another_window);
-
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
-
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
-            ImGui::End();
-        }
-
-        // 3. Show another simple window.
-        if (show_another_window)
-        {
-            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-            ImGui::Text("Hello from another window!");
-            if (ImGui::Button("Close Me"))
-                show_another_window = false;
             ImGui::End();
         }
 
